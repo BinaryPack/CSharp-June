0babee22049b3bcc7c9e540cb3e67724ee648a5a
BinaryPack <thedanielmorrison@gmail.com>
2026-06-28 18:57:41 +0300

    Я съел деда задача от 28.06.26

---
+++ WorkProject (28.06.26)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var producer = new Product("Стивен", 2000m);
+            Console.WriteLine(producer.Name);
+        }
+    }
+    public class Product
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            if (price >= 0) Price = price;
+            else throw new Exception("Цена не может быть отрицательной");
+        }
+
+    }
+}
